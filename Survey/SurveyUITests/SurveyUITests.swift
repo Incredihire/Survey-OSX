10ed856abab5ed7a8e6e9254169a0e10c3c0a762
DarynaBorzovets <biz.daryna@gmail.com>
2024-09-19 00:10:59 -0400

    Declaration can not be market static.

--- Survey/SurveyUITests/SurveyUITests.swift
+++ Survey/SurveyUITests/SurveyUITests.swift
@@ -1,25 +1,26 @@
 import XCTest
 
-static class SurveyUITestsLaunchTests: XCTestCase {
-
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
-        true
-    }
+final class SurveyUITests: XCTestCase {
 
     override func setUpWithError() throws {
         continueAfterFailure = false
     }
 
-    func testLaunch() throws {
+    override func tearDownWithError() throws {
+        // Teardown code here if needed
+    }
+
+    func testExample() throws {
         let app = XCUIApplication()
         app.launch()
+        // Add assertions here
+    }
 
-        // Insert steps here to perform after app launch but before taking a screenshot,
-        // such as logging into a test account or navigating somewhere in the app
-
-        let attachment = XCTAttachment(screenshot: app.screenshot())
-        attachment.name = "Launch Screen"
-        attachment.lifetime = .keepAlways
-        add(attachment)
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
     }
 }
