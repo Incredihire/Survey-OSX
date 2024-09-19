496d576392629564c50c5947f100f7e8fdc390e0
DarynaBorzovets <biz.daryna@gmail.com>
2024-09-18 23:50:46 -0400

    Codacy

--- Survey/SurveyUITests/SurveyUITests.swift
+++ Survey/SurveyUITests/SurveyUITests.swift
@@ -3,25 +3,21 @@ import XCTest
 final class SurveyUITests: XCTestCase {
 
     override func setUpWithError() throws {
-   
         continueAfterFailure = false
-
     }
 
     override func tearDownWithError() throws {
-    
+        // Teardown code here if needed
     }
 
     func testExample() throws {
-        // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-
+        // Add assertions here
     }
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-         
             measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
             }
