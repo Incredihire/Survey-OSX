6bc4d47852d196e1e86d0fa2bdd0a65d33e52d3b
Daryna Dash <86532703+DarynaBorzovets@users.noreply.github.com>
2024-09-24 22:24:43 -0400

    Codacy

--- Survey/Survey/Network/MockServer.swift
+++ Survey/Survey/Network/MockServer.swift
@@ -6,7 +6,6 @@ class MockServer: InquiryService {
             let error = NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "File not found"])
             return Fail(error: error).eraseToAnyPublisher()
         }
-        
         return URLSession.shared.dataTaskPublisher(for: url)
             .map { $0.data }
             .decode(type: [Inquiry].self, decoder: JSONDecoder())
