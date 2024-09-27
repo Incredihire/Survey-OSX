2951738aa565dacce2657f834c2a767925710833
Daryna Dash <86532703+DarynaBorzovets@users.noreply.github.com>
2024-09-27 13:56:37 -0400

    Codacy

--- Survey/Survey/Logger.swift
+++ Survey/Survey/Logger.swift
@@ -6,16 +6,18 @@ class Logger {
     private let log: OSLog
 
     private init() {
-        self.log = OSLog(subsystem: Bundle.main.bundleIdentifier ?? "Incredihire-LLC.Survey", category: "MockServer")
+        self.log = OSLog(
+            subsystem: Bundle.main.bundleIdentifier ?? "Incredihire-LLC.Survey",
+            category: "MockServer"
+        )
     }
-
-    func log(error: Error, customMessage: String? = nil) {
-        let message = customMessage != nil ? "\(customMessage!): \(error.localizedDescription)" : error.localizedDescription
+ func log(error: Error, customMessage: String? = nil) {
+        let message = customMessage != nil
+            ? "\(customMessage!): \(error.localizedDescription)"
+            : error.localizedDescription
         os_log("Error logged: %@", log: log, type: .error, message)
     }
-
-    func log(message: String) {
+ func log(message: String) {
         os_log("%@", log: log, type: .info, message)
     }
 }
-
