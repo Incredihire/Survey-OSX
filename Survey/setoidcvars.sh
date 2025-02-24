1ac806c7f8db75e5c1edf8bb6a5766c33644d26a
Jereme Willig <jereme@jeremewillig.com>
2025-02-24 13:01:19 -1000

    SR-152-User-should-be-able-to-log-into-Survey-via-the-OSX-app added directions and example for setting OIDC environment variables in ~/.zshenv file

--- Survey/setoidcvars.sh
+++ Survey/setoidcvars.sh
@@ -1,3 +1,8 @@
+# NOTE: please set environment variables in ~/.zshenv like example lines below before building
+#export OIDC_ISSUER="https:\/\/accounts.google.com"
+#export OIDC_REDIRECT_URI="com.googleusercontent.apps.0123456789012-abcdef3456g7hi89jk0lm12nop34qr5s:/auth/callback"
+#export OIDC_CLIENT_ID="0123456789012-abcdef3456g7hi89jk0lm12nop34qr5s.apps.googleusercontent.com"
+#export OIDC_CLIENT_SECRET="ABCDEF-GHIj0kLMN1p23QRSTuv4wxYzaBcD"
 sed -i "" "s OIDC_ISSUER_HERE $OIDC_ISSUER g " $PROJECT_DIR/AppDelegate.swift;
 sed -i ""  "s OIDC_REDIRECT_URI_HERE $OIDC_REDIRECT_URI g " $PROJECT_DIR/AppDelegate.swift;
 sed -i ""  "s/OIDC_CLIENT_ID_HERE/$OIDC_CLIENT_ID/g" $PROJECT_DIR/AppDelegate.swift;
